3850bff134e48904945f7855134923eadb679f64
Bruno <brunomluque@gmail.com>
2024-10-06 20:41:54 -0600

    Test QA

--- src/components/Layouts/Banner.js
+++ src/components/Layouts/Banner.js
@@ -34,7 +34,7 @@ export default function Banner() {
                 onClick={handleOpenModal}
                 class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
               >
-                {"Start Free Trial"}
+                {"Start Free Trial."}
               </button>
             </div>
             {/* <p class="text-sm md:text-lg mb-8">{t("startupValuationWebsiteSubtitle")}</p> */}
