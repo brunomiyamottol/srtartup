a4ed396a9006cb6ec076bbd266580c9c1fb03479
Bruno <brunomluque@gmail.com>
2024-10-07 16:54:18 -0600

    Added ANalytics GA_TRACKING ID_ FIXED ERROR NOT DEFINED

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
+import { GA_TRACKING_ID } from "../utils/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -49,6 +50,23 @@ class MyDocument extends Document {
         `,
           }}
         />
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.GA_TRACKING_ID}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+            }}
+          />
           <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
